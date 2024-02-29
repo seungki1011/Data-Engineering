e95a6f730337bd3fa5a687107501750fea517f48
seungki1011 <kimseungki1011@gmail.com>
2024-02-29 23:31:37 +0900

    Docs: Add example code for spring core

---
+++ spring/springbasic1/src/test/java/de/springbasic1/singleton/SingletonService.java
@@ -0,0 +1,18 @@
+package de.springbasic1.singleton;
+
+public class SingletonService {
+    // 1. static 영역에 객체를 딱 1개만 생성
+    private static final SingletonService instance = new SingletonService();
+
+    // 2. public으로 열어서 인스턴스가 필요하면 이 static 메서드를 통해서만 조회하도록 허용
+    public static SingletonService getInstance() {
+        return instance;
+    }
+
+    // 3. 생성자를 private으로 선언해서 외부에서 new를 사용하지 못하도록 막는다
+    private SingletonService() {}
+
+    public void logic() {
+        System.out.println("싱글톤 객체 로직 호출");
+    }
+}

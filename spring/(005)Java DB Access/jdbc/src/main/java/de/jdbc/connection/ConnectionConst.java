7a37d113340e6df2f55482e3651fae740715d0de
seungki1011 <kimseungki1011@gmail.com>
2024-04-23 21:10:00 +0900

    Add jdbc example code

---
+++ spring/(005)Java DB Access/jdbc/src/main/java/de/jdbc/connection/ConnectionConst.java
@@ -0,0 +1,8 @@
+package de.jdbc.connection;
+
+public abstract class ConnectionConst {
+    public static final String URL = "jdbc:mysql://localhost:3306/test_database?serverTimezone=Asia/Seoul";
+    public static final String USERNAME = "root";
+    public static final String PASSWORD = "admin";
+
+}

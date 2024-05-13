597999a0a3f556d4b697ef79eeb5a4f9407f93bc
seungki1011 <kimseungki1011@gmail.com>
2024-05-13 20:26:53 +0900

    Add spring advanced

---
+++ spring/(005)SpringAdvanced/logtracker/src/main/java/de/logtracker/LogtrackerApplication.java
@@ -0,0 +1,13 @@
+package de.logtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogtrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogtrackerApplication.class, args);
+	}
+
+}
---
+++ spring/(005)SpringAdvanced/logtracker/src/test/java/de/logtracker/LogtrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package de.logtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogtrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

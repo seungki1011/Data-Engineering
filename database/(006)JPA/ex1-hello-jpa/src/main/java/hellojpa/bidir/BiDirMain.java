77783f1838aba3ad6b157b751a75b723f1cec355
seungki1011 <kimseungki1011@gmail.com>
2024-06-10 23:17:27 +0900

    Update jpa

--- database/(006)JPA/ex1-hello-jpa/src/main/java/hellojpa/bidir/BiDirMain.java
+++ database/(006)JPA/ex1-hello-jpa/src/main/java/hellojpa/bidir/BiDirMain.java
@@ -1,11 +1,11 @@
 package hellojpa.bidir;
 
-import hellojpa.unidir.UniDirMember;
-import hellojpa.unidir.UniDirTeam;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
+
+import java.util.List;
 
 public class BiDirMain {
     public static void main(String[] args) {
@@ -18,10 +18,21 @@ public class BiDirMain {
 
         try {
 
+            BiDirTeam team = new BiDirTeam("TeamA");
+            em.persist(team);
 
+            BiDirMember member = new BiDirMember("member1", team);
+            em.persist(member);
 
-             em.flush();
-             em.clear();
+            em.flush();
+            em.clear();
+
+            BiDirMember findMember = em.find(BiDirMember.class, member.getId());
+            List<BiDirMember> members = findMember.getTeam().getMembers();
+
+            for (BiDirMember m : members) {
+                System.out.println("m.getUsername() = " + m.getUsername());
+            }
 
             tx.commit();
         } catch (RuntimeException e) {
